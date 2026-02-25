5eea99a916ac5fae2237b0c34778b30aac3e7c8e
Siam Sarkar <opsiamop10@gmail.com>
2026-02-25 18:59:18 +0600

    Fix production CORS and API issues and some other issue2

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ require('dotenv').config();
 
 const app = express();
 
-// CORS configuration
-app.use(cors({
-  origin: ['https://kdpo.onrender.com', 'http://localhost:5173', 'http://localhost:3000'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
